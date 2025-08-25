3830a32d3a526714272525f8f356c927d4589409
Gaelpem <pembelegael@gmail.com>
2025-08-25 20:37:22 +0200

    commit3

--- brouillon.c
+++ brouillon.c
@@ -2,19 +2,33 @@
 #include <stdbool.h>
 #include <ctype.h>
 #include <string.h>
+
+
+
 #include <unistd.h>
 
 
-void ft_print_alphabet(void){
-  for(char c = 'a'; c <= 'z'; c++){ write(1,&c, 1);}
- 
+void ft_putchar(char c){
+     write(1,&c,1); 
+}
+
+
+void ft_putnumber(int n ){
+       if(n >= 10){
+           ft_putchar('0' + n/10); 
+       }
+        ft_putchar('0' + n%10); 
+  
+}
+
+void ft_print_number(void){
+     int i = 0; 
+     while(i<= 15){
+          ft_putnumber(i); 
+          i++; 
+     }
 }
 
 int main(){
-  ft_print_alphabet(); 
+     ft_print_number(); 
 }
-   
-   
-    
-
-
